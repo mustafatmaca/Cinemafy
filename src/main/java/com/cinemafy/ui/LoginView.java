2429c5f0521657019ae9fa55cf5a5d7333897bb2
mustafatmaca <mustafaatmaca362@gmail.com>
2021-06-10 15:12:57 +0300

    initial commit

---
+++ src/main/java/com/cinemafy/ui/LoginView.java
@@ -0,0 +1,9 @@
+package com.cinemafy.ui;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+public class LoginView extends VerticalLayout {
+
+}
--- src/main/java/com/cinemafy/ui/MainView.java
+++ src/main/java/com/cinemafy/ui/MainView.java
@@ -5,7 +5,7 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 
-@Route("")
+@Route("/main")
 public class MainView extends VerticalLayout {
 
     public MainView() {

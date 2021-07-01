35e990cceac6b2cafeb77e49e972d99124f7d329
mustafatmaca <mustafaatmaca362@gmail.com>
2021-07-01 15:07:03 +0300

    initial commit

--- src/main/java/com/cinemafy/ui/RegisterView.java
+++ src/main/java/com/cinemafy/ui/RegisterView.java
@@ -3,8 +3,12 @@ package com.cinemafy.ui;
  * @author Mustafa Atmaca
  */
 
+import com.cinemafy.backend.models.User;
+import com.cinemafy.backend.services.UserService;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
@@ -12,8 +16,10 @@ import com.vaadin.flow.router.Route;
 
 @Route("/register")
 public class RegisterView extends VerticalLayout {
+    private final UserService userService;
 
-    public RegisterView() {
+    public RegisterView(UserService userService) {
+        this.userService = userService;
         H2 title = new H2("Sign Up");
 
         TextField firstNameField = new TextField("First Name");
@@ -23,6 +29,16 @@ public class RegisterView extends VerticalLayout {
 
         Button button = new Button("Sign Up");
 
+        button.addClickListener(event -> {
+            if (!firstNameField.getValue().isEmpty() && !lastNameField.getValue().isEmpty() && !emailField.getValue().isEmpty() && !passwordField.getValue().isEmpty()){
+                register(firstNameField.getValue(), lastNameField.getValue(), emailField.getValue(), passwordField.getValue());
+                UI.getCurrent().getPage().setLocation("/login");
+            }
+            else {
+                Notification.show("Alanları Boş Bırakmayınız!");
+            }
+        });
+
         setAlignItems(Alignment.CENTER);
         setHorizontalComponentAlignment(Alignment.STRETCH, firstNameField, lastNameField,
                 emailField, passwordField, button);
@@ -30,5 +46,14 @@ public class RegisterView extends VerticalLayout {
         getStyle().set("margin", "0 auto");
 
         add(title, firstNameField, lastNameField, emailField, passwordField, button);
+    }
+
+    private void register(String fn, String ln, String mail, String pw) {
+        User user = new User();
+        user.setFirstName(fn);
+        user.setLastName(ln);
+        user.setEmail(mail);
+        user.setPassword(pw);
+        userService.save(user);
     }
 }
